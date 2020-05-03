30c57487fa904e19c116229c6c15c37e16934d2c
Kreativnoime123 <61324535+Kreativnoime123@users.noreply.github.com>
2020-05-03 17:30:57 +0200

    Added assert tester comet date sorter

---
+++ testCometDateSorter.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "CometDataSorter.h"
+#include <cassert>
+#include <vector>
+
+
+void testCometDateSorter(){
+    vector <numberOfCometsInADay> testVector;
+    testVector = fNumberOfCometsInADay();
+    assert(testVector[0].day == 21, "Day ne radi normalno");
+    assert(testVector[0].month == 10, "Month ne radi normalno");
+    assert(testVector[0].year == 2010, "Year ne radi normalno");
+    assert(testVector[0].numberOfComets == 36, "NumberOfComets ne radi normalno");
+    assert(testVector[1].day == 26, "Day ne radi normalno");
+    assert(testVector[1].month == 10, "Month ne radi normalno");
+    assert(testVector[1].year == 2010, "Year ne radi normalno");
+    assert(testVector[1].numberOfComets == 19, "NumberOfComets ne radi normalno");
+    assert(testVector[2].day == 3, "Day ne radi normalno");
+    assert(testVector[2].month == 11, "Month ne radi normalno");
+    assert(testVector[2].year == 2010, "Year ne radi normalno");
+    assert(testVector[2].numberOfComets == 26, "NumberOfComets ne radi normalno");
+}
